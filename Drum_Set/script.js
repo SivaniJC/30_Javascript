558c13bd2849257d108cfa13d80da3beb3e08e7b
Sivani JC <68504148+SivaniJC@users.noreply.github.com>
2021-08-18 18:17:42 +0530

    Add files via upload

---
+++ Drum_Set/script.js
@@ -0,0 +1,22 @@
+const removeTransition = event =>
+    event.propertyName !== 'transform' ? null : event.target.classList.remove('playing');
+
+const addTransition = event =>{
+    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+    key.classList.add('playing');
+};
+    
+const keys = Array.from(document.querySelectorAll('.key'));
+  keys.map(key => key.addEventListener('transitionend', removeTransition));
+
+const playSound = event => {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    addTransition(event);
+};
+
+window.addEventListener('keydown', playSound);
